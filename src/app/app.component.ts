19f7d280e2733f95029bcefd16284dcde70f6b69
bomotayo <omotayobabatundee@gmail.com>
2021-11-11 20:21:14 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      username: 'nature',
+      content: 'I saw this neat tree today',
+      imageUrl: 'assets/tree.jpeg',
+    },
+    {
+      title: 'Snowy Mountain',
+      username: 'hikingperson',
+      content: 'Here is a picture of a snowy mountain',
+      imageUrl: 'assets/mountain.jpeg',
+    },
+    {
+      title: 'Neat Tree',
+      username: 'biking12222',
+      content: 'Action shot of me riding my bike',
+      imageUrl: 'assets/biking.jpeg',
+    },
+  ];
 }
